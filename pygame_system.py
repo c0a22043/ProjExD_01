a6f5142f5ca1bfdf5fc2bb1c2f4dc04938b0e8e4
c0a22043 <c0a2204357@edu.teu.ac.jp>
2023-09-26 14:42:41 +0900

    練習問題1

--- pygame_system.py
+++ pygame_system.py
@@ -2,27 +2,27 @@ import pygame as pg
 import sys
 
 def main():
-    pg.display.set_caption("はじめてのPygame")
-    screen = pg.display.set_mode((800, 600))
+    pg.display.set_caption("はばたけ！こうかとん！")
+    screen = pg.display.set_mode((1600, 900))
     clock = pg.time.Clock()
     font = pg.font.Font(None, 80)
-
-    enn = pg.Surface((20, 20))
-    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
-    enn.set_colorkey((0, 0, 0))
+    background = pg.image.load("ex01/fig/pg_bg.jpg")
 
     tmr = 0
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: return
-        
+            if event.type == pg.QUIT:
+                return
+
+        screen.blit(background, (0, 0))
+
         txt = font.render(str(tmr), True, (255, 255, 255))
-        screen.fill((50, 50, 50))
-        screen.blit(txt, [300, 200])
-        screen.blit(enn, [100, 400])
+        screen.blit(txt, [100, 100])
+        
         pg.display.update()
-        tmr += 1        
+        tmr += 1
         clock.tick(1)
+
 
 
 if __name__ == "__main__":
