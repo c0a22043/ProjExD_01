6c1a15ec444f84b22241b9a992624b5393268e9e
c0a22043 <c0a2204357@edu.teu.ac.jp>
2023-09-26 15:20:04 +0900

    3

--- pygame_system.py
+++ pygame_system.py
@@ -2,34 +2,28 @@ import pygame as pg
 import sys
 
 def main():
-    pg.display.set_caption("はばたけ！こうかとん！")
-    screen = pg.display.set_mode((1600, 900))
+    pg.display.set_caption("はじめてのPygame")
+    screen = pg.display.set_mode((800, 600))
     clock = pg.time.Clock()
     font = pg.font.Font(None, 80)
-    background = pg.image.load("ex01/fig/pg_bg.jpg")
-    bird = pg.image.load("ex01/fig/3.png")
-    bird_flipped = pg.transform.flip(bird, True, False)
 
-
+    enn = pg.Surface((20, 20))
+    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
+    enn.set_colorkey((0, 0, 0))
 
     tmr = 0
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT:
-                return
-
-        screen.blit(background, (0, 0))
-
+            if event.type == pg.QUIT: return
         
-        screen.blit(bird_flipped, (400, 200))
-
         txt = font.render(str(tmr), True, (255, 255, 255))
-        screen.blit(txt, [100, 100])
-
-        
+        screen.fill((50, 50, 50))
+        screen.blit(txt, [300, 200])
+        screen.blit(enn, [100, 400])
         pg.display.update()
-        tmr += 1
+        tmr += 1        
         clock.tick(1)
+
 
 
 
