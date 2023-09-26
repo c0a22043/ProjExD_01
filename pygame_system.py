2f643de608b3c541bf67552ebdec4de13b553b76
c0a22043 <c0a2204357@edu.teu.ac.jp>
2023-09-26 14:57:56 +0900

    練習問題2

--- pygame_system.py
+++ pygame_system.py
@@ -7,6 +7,10 @@ def main():
     clock = pg.time.Clock()
     font = pg.font.Font(None, 80)
     background = pg.image.load("ex01/fig/pg_bg.jpg")
+    bird = pg.image.load("ex01/fig/3.png")
+    bird_flipped = pg.transform.flip(bird, True, False)
+
+
 
     tmr = 0
     while True:
@@ -16,8 +20,12 @@ def main():
 
         screen.blit(background, (0, 0))
 
+        
+        screen.blit(bird_flipped, (400, 200))
+
         txt = font.render(str(tmr), True, (255, 255, 255))
         screen.blit(txt, [100, 100])
+
         
         pg.display.update()
         tmr += 1
