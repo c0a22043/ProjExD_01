3b8a32c9e14b94cea0d0386a9fdb5042b7fdd83b
c0a22043 <c0a2204357@edu.teu.ac.jp>
2023-09-26 17:51:03 +0900

    雲の連続性

--- pygame_image.py
+++ pygame_image.py
@@ -7,30 +7,35 @@ def main():
     screen = pg.display.set_mode((1600, 900))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img_flipped = pg.transform.flip(bg_img, True, False)
     bird_flipped = pg.image.load("ex01/fig/3.png")
     bird_flipped = pg.transform.flip(bird_flipped, True, False)
     bird_rotozoom = pg.transform.rotozoom(bird_flipped, 10,1.0)
     bird_images = [bird_flipped, bird_rotozoom]
+
     tmr = 0
     bird_idx = 0
     bird_x, bird_y = 300, 200
-    x = 1599
+    x = 1600
     bird_speed = 3
     flap_delay = 20
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         x -= bird_speed
         if x < 0:
-            x = 1599
+            x = 1600
 
         screen.blit(bg_img, [x, 0])
-        screen.blit(bg_img, (x - 1600, 0))
-        screen.blit(bird_images[bird_idx], (bird_x, bird_y))
+        screen.blit(bg_img_flipped, (x - 1600, 0))
 
         if tmr % flap_delay == 0:
-                    bird_idx = 1 - bird_idx
+            bird_idx = 1 - bird_idx
+        
+
+        screen.blit(bird_images[bird_idx], (bird_x, bird_y))
 
         pg.display.update()
         tmr += 1        
