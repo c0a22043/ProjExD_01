e64dbd74c6c37f8a4b8661444adfb0b16fb663cc
c0a22043 <c0a2204357@edu.teu.ac.jp>
2023-09-26 15:19:27 +0900

    2

---
+++ pygame_image.py
@@ -0,0 +1,30 @@
+import sys
+import pygame as pg
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((1600, 900))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bird_flipped = pg.image.load("ex01/fig/3.png")
+    bird_flipped = pg.transform.flip(bird_flipped, True, False)
+    bird_rotated = pg.transform.rotate(bird_flipped, 10)
+    bird_images = [bird_flipped, bird_rotated]
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        screen.blit(bird_images[0], (300, 200))
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
