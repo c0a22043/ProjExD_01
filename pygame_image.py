e73854a86026e841a3a66ee9b8dcce5e3e269563
c0a22043 <c0a2204357@edu.teu.ac.jp>
2023-09-26 15:48:00 +0900

    6

--- pygame_image.py
+++ pygame_image.py
@@ -9,21 +9,25 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     bird_flipped = pg.image.load("ex01/fig/3.png")
     bird_flipped = pg.transform.flip(bird_flipped, True, False)
-    bird_rotated = pg.transform.rotate(bird_flipped, 10)
-    bird_images = [bird_flipped, bird_rotated]
+    bird_rotozoom = pg.transform.rotozoom(bird_flipped, 10,1.0)
+    bird_images = [bird_flipped, bird_rotozoom]
     tmr = 0
-    idx = 0  
     bird_x, bird_y = 300, 200
+    x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(bird_images[idx], (bird_x, bird_y))
-        idx = 1 - idx
+        x -= 1
+        if x < 0:
+            x = 1599
+
+        screen.blit(bg_img, [x, 0])
+        screen.blit(bg_img, (x - 1600, 0))
+        screen.blit(bird_images[tmr%2], (bird_x, bird_y))
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
